7e36dda1b69d9936bd8c717fb64d5105a9c279f7
MCHsu <a860610@gmail.com>
2017-03-29 09:21:43 +0800

    Update tipbox.js

--- tipbox.js
+++ tipbox.js
@@ -1,4 +1,4 @@
- /*goole map*/ 
+/*goole map*/ 
  function initMap() {
         var myLatLng = {lat: 22.979919, lng: 120.220703};
 
@@ -14,23 +14,24 @@
           title: 'Hello World!'
         });
       }
-
-
-  $(function(){
+/*navbar*/
+ $(function(){
 　$(window).load(function(){
 　　$(window).bind('scroll resize', function(){
 　　var $this = $(this);
 　　var $this_Top=$this.scrollTop();
     var $p = $(".nav");
+    
 　　if($this_Top < 30){
 　　　$('.nav').stop().animate({top:"0px"});
 　　　$p.css("background-color","rgba(0, 0, 0, 0)");
   }
+    
 　　　　if($this_Top > 30){
 　　　　$('.nav').stop().animate({top:"-40px"});
         $p.css("background-color","rgba(26, 23, 24, 0.9)");
 　　　 }
 　　}).scroll();
 　});
-　}); 
-
+});
+/**/
